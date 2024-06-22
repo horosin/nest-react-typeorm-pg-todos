1083d093be0877f56d447ba76a27f3ef60e41b14
Karol Horosin <karol.horosin@gmail.com>
2024-06-22 22:27:26 +0200

    User creation endpoint

--- server/src/users/user.entity.ts
+++ server/src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Unique, BeforeInsert } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
 @Entity()
@@ -16,13 +16,14 @@ export class User {
   @Column()
   salt: string;
 
+  @BeforeInsert()
+  async hashPassword() {
+    this.salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, this.salt);
+  }
+
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt);
     return hash === this.password;
-  }
-
-  async setPassword(password: string): Promise<void> {
-    this.salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(password, this.salt);
   }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -22,7 +23,8 @@ export class UsersService {
     return this.usersRepository.findOne({ where: { email } });
   }
 
-  async create(user: User): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
     return this.usersRepository.save(user);
   }
 
